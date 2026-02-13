c3938e3d48715cfdea31b694a6a38abf94f9e695
23aneessss <oa_bouziani@esi.dz>
2026-02-13 00:12:08 -0800

    working on search page

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,62 @@
+import { Text, View , Image, FlatList, ActivityIndicator } from 'react-native'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/api'
+import { useRouter } from 'expo-router'
+import { icons } from '@/constants/icons'
+import  SearchBar  from '@/components/SearchBar'
+
+const search = () => {
+
+    const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+  return (
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className='flex-1 
+      absolute w-full z-0' resizeMode='cover' />
+
+      <FlatList
+            data={movies}
+            renderItem={({ item }) => <MovieCard {...item}/> }
+            keyExtractor={(item) => item.id.toString()}
+            className='px-5'
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: 'center',
+              gap: 16,
+              marginVertical: 16
+            }}
+
+            contentContainerStyle={{ paddingBottom: 100 }}
+            ListHeaderComponent={
+              <>
+
+                <View className='w-full flex-row justify-center mt-20 items-center'>
+                    <Image source={icons.logo} className='w-12 h-10'/>
+                </View>
+
+                <View className='my-5'>
+                  <SearchBar placeholder='Search Movies ...'/>
+                </View>
+
+                {moviesLoading && (
+                    <ActivityIndicator size='large' color='#0000ff' 
+                    className='my-3' />
+                )}
+
+              </>
+            }
+      />
+
+    </View>
+  )
+}
+
+export default search
